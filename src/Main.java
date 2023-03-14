fd743fae750061811af02d84f4f7635d762371ec
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-14 13:46:11 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in en temperatur i Fahrenheit och omvanda den till Celsius
+        // Man kan använda formeln: C = (F-32)*5/9
+
+
+    }
+}
