5648928028b48231d7ee8e9f4abe6990001e7212
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-14 13:52:49 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Läs in en temperatur i Fahrenheit och omvanda den till Celsius
         // Man kan använda formeln: C = (F-32)*5/9
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ange en temperatur i Fahrenheit: ");
+        double f = input.nextDouble();
+        double c = (f-32)*5/9;
 
+        System.out.println("Omvandlat till celsius blir det " + c +  " grader.");
     }
 }
